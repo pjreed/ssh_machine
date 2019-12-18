f2a0b80543a71246cf0373728c88c25aeb0c51c9
P. J. Reed <preed@swri.org>
2019-12-18 12:49:25 -0600

    Initial commit

---
+++ launch/pub_sub.launch.py
@@ -0,0 +1,41 @@
+# Copyright 2018 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Launch a talker and a listener."""
+import launch_ros
+from launch import LaunchDescription, LaunchIntrospector
+from launch.local_machine import LocalMachine
+
+
+def generate_launch_description():
+    ld = LaunchDescription([
+        launch_ros.actions.Node(
+            package='demo_nodes_cpp', node_executable='talker', output='screen',
+            remappings=[('chatter', 'my_chatter')],
+            machine=LocalMachine(hostname="grandgrowl.local")),
+        launch_ros.actions.Node(
+            package='demo_nodes_cpp', node_executable='listener', output='screen',
+            remappings=[('chatter', 'my_chatter')]),
+    ])
+
+    print('Starting introspection of launch description...')
+    print('')
+
+    print(LaunchIntrospector().format_launch_description(ld))
+
+    print('')
+    print('Starting launch of launch description...')
+    print('')
+
+    return ld
---
+++ setup.py
@@ -0,0 +1,28 @@
+import os
+from glob import glob
+from setuptools import setup
+
+package_name = 'swri_launch_test'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        (os.path.join('share', package_name), ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*.launch.py'))
+    ],
+    install_requires=['setuptools', 'launch'],
+    zip_safe=True,
+    maintainer='P. J. Reed',
+    maintainer_email='preed@swri.org',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
